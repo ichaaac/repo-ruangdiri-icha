5a9b1d3bbc730fac9d15fa10de2aadb76f3fa7e0
muhammadadmiral <admelar15@gmail.com>
2025-05-16 09:29:33 +0700

    Chore : remove use client

--- src/pages/organization/school/SchoolDashboard.jsx
+++ src/pages/organization/school/SchoolDashboard.jsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { PieChart, Pie, Cell, BarChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Tooltip } from 'recharts';
